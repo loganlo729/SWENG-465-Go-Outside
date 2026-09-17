7c0bfe9587351c88e9ef790f8969aa3b1d35dce1
loganlo729 <loganlo729@gmail.com>
2026-09-17 11:08:38 -0400

    Added Node Server with GET and POST requests for Events

---
+++ RESTful API Project/server.js
@@ -0,0 +1,42 @@
+// Import the express library
+const express = require('express'); 
+
+// Initialize the Express application
+const app = express();
+
+
+app.use(express.json()); 
+
+// Create basic 
+let events = [];
+
+// Define the port the server will listen on
+const PORT = 10533; 
+
+// GET request for events
+app.get('/api/events', (req, res) => {
+	res.send(events);
+});
+
+// POST request for events
+app.post('/api/events', (req, res) => {
+	const {eventName, category, host} = req.body;
+	const newEvent = {
+        	eventName: eventName,
+        	category: category,
+        	host: host
+    	};
+	// Push to array
+	events.push(newEvent);
+
+	// Return status
+	res.status(201).json({
+		message: 'Event created successfully',
+		event: newEvent
+	});
+});
+
+// Tell the server to listen for incoming network requests
+app.listen(PORT, () => {
+	console.log(`Server is running at http://localhost:${PORT}`);
+});

302100ef213651f2b1b8830dcd72323bb17a75c4
avatonini22 <act5590@psu.edu>
2026-09-17 11:30:33 -0400

    Added validation for community POST request.
    Added additional comments to code

--- RESTful API Project/server.js
+++ RESTful API Project/server.js
@@ -1,3 +1,4 @@
+// server.js
 // Import the express library
 const express = require('express'); 
 
@@ -7,64 +8,76 @@ const app = express();
 
 app.use(express.json()); 
 
-// Create basic 
+// Create basic arrays as our "Databases"
 let events = [];
 let communities = [];
 
 // Define the port the server will listen on
 const PORT = 10533; 
 
-// GET request for events
+// GET endpoint for all events
 app.get('/api/events', (req, res) => {
 	res.send(events);
 });
 
-// POST request for events
+// POST endpoint to create a new event
 app.post('/api/events', (req, res) => {
 	const {eventName, category, host} = req.body;
 
+	// Require fields for validation
 	if (!eventName || !category || !host) {
 		return res.status(400).send('Missing required fields.');
 	}
 
+	// Create newEvent object
 	const newEvent = {
         	eventName: eventName,
         	category: category,
         	host: host
     	};
+
 	// Push to array
 	events.push(newEvent);
 
-	// Return status
+	// Return success status
 	res.status(201).json({
 		message: 'Event created successfully',
 		event: newEvent
 	});
 });
 
-// GET request for communities
+// GET endpoint for all communities
 app.get('/api/communities', (req, res) => {
 	res.send(communities);
 });
 
-// POST request for communities
+// POST endpoint to create a new community
 app.post('/api/communities', (req, res) => {
 	const {communityName, category, host} = req.body;
+	
+	// Require fields for validation
+	if (!communityName || !category || !host) {
+		return res.status(400).send('Missing required fields.');
+	}
+	
+	// Create newCommunity object
 	const newCommunity = {
         	communityName: communityName,
         	category: category,
         	host: host
     	};
+
 	// Push to array
 	communities.push(newCommunity);
 
-	// Return status
+	// Return success status
 	res.status(201).json({
 		message: 'Community created successfully',
 		community: newCommunity
 	});
 });
 
+// Start the server
 // Tell the server to listen for incoming network requests
 app.listen(PORT, () => {
 	console.log(`Server is running at http://localhost:${PORT}`);
